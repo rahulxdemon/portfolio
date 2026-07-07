fe394f8c4a75836874958d5ae8792baa9e7a4d02
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-07-07 23:00:16 +0530

    feat: add initial scramble text to the nav links

--- src/features/header/components/base-header.tsx
+++ src/features/header/components/base-header.tsx
@@ -1,16 +1,33 @@
 import { Link } from '@tanstack/react-router';
-import { NAV_LINKS } from './data';
+import { useScramble } from 'use-scramble';
+import { type INavLink, NAV_LINKS } from './data';
 
 export function BaseHeader() {
   return (
     <header className='px-[2ch] py-mono'>
       <nav className='flex items-center justify-start gap-4'>
         {NAV_LINKS.map((link) => (
-          <Link key={link.id} {...link.linkOptions}>
-            [{link.label}]
-          </Link>
+          <NavLink key={link.id} link={link} />
         ))}
       </nav>
     </header>
+  );
+}
+
+function NavLink({ link }: { link: INavLink }) {
+  // hook returns a ref
+  const { ref, replay } = useScramble({
+    text: link.label,
+    speed: 0.6, // Explicitly set the base speed (default is 1)
+    tick: 1, // Reduce this so it advances quickly along the string
+    step: 1, // Ensures it introduces 1 new character position per redraw
+    scramble: 10, // Increase this so characters shuffle longer before resolving
+    overdrive: false,
+  });
+
+  return (
+    <Link ref={ref} key={link.id} {...link.linkOptions} onMouseOver={replay} onFocus={replay}>
+      {link.label}
+    </Link>
   );
 }
--- src/features/header/components/data.ts
+++ src/features/header/components/data.ts
@@ -1,36 +1,36 @@
 import { type LinkOptions, linkOptions } from '@tanstack/react-router';
 
-export interface NavLink {
+export interface INavLink {
   id: string;
   label: string;
   linkOptions: LinkOptions;
 }
 
-export const NAV_LINKS: NavLink[] = [
+export const NAV_LINKS: INavLink[] = [
   {
     id: 'about',
-    label: 'about',
+    label: '[about]',
     linkOptions: linkOptions({
       to: '/',
     }),
   },
   {
     id: 'cv',
-    label: 'cv',
+    label: '[cv]',
     linkOptions: linkOptions({
       to: '/cv',
     }),
   },
   {
     id: 'blogs',
-    label: 'blogs',
+    label: '[blogs]',
     linkOptions: linkOptions({
       to: '/blogs',
     }),
   },
   {
     id: 'quotes',
-    label: 'quotes',
+    label: '[quotes]',
     linkOptions: linkOptions({
       to: '/quotes',
     }),
