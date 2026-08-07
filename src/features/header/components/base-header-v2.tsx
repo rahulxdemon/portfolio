3c05a3756ed5b21bca52f1ddaf9463aa45429856
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-08-07 19:24:23 +0530

    feat: add individual scramble text for header

--- src/features/header/components/base-header-v2.tsx
+++ src/features/header/components/base-header-v2.tsx
@@ -4,7 +4,7 @@ import { type INavLink, NAV_LINKS } from './data';
 
 export function BaseHeaderV2() {
   return (
-    <header className='fixed top-0 right-0 px-[2ch] py-[1.5ch]'>
+    <header className='fixed top-0 right-0 px-[2ch] py-[2ch]'>
       <nav className='flex items-start justify-start gap-0 flex-col w-25.25 font-chivo font-medium'>
         {NAV_LINKS.map((link, index) => (
           <NavLink key={link.id} link={link} index={index} />
@@ -15,9 +15,8 @@ export function BaseHeaderV2() {
 }
 
 function NavLink({ link, index }: { link: INavLink; index: number }) {
-  // hook returns a ref
-  const { ref, replay } = useScramble({
-    text: `0${index + 1}_ ${link.label}`,
+  const { ref: indexRef, replay: indexReplay } = useScramble({
+    text: `0${index}_`,
     speed: 0.6, // Explicitly set the base speed (default is 1)
     tick: 1, // Reduce this so it advances quickly along the string
     step: 1, // Ensures it introduces 1 new character position per redraw
@@ -25,20 +24,37 @@ function NavLink({ link, index }: { link: INavLink; index: number }) {
     overdrive: false,
   });
 
+  // hook returns a ref
+  const { ref, replay } = useScramble({
+    text: `${link.label}`,
+    speed: 0.6, // Explicitly set the base speed (default is 1)
+    tick: 1, // Reduce this so it advances quickly along the string
+    step: 1, // Ensures it introduces 1 new character position per redraw
+    scramble: 10, // Increase this so characters shuffle longer before resolving
+    overdrive: false,
+  });
+
+  function handleReplay() {
+    replay();
+    indexReplay();
+  }
+
   return (
     <Link
       key={link.id}
-      ref={ref}
-      className='focus-visible:outline-text/60 w-full focus-visible:outline-1 uppercase transition-colors duration-300 py-[0.35ch]'
+      className='focus-visible:outline-text/60 w-full group focus-visible:outline-1 uppercase transition-colors duration-300 py-[0.35ch]'
       {...link.linkOptions}
       activeProps={{ className: 'text-text' }}
       inactiveProps={{ className: 'text-text/65 hover:text-text/90' }}
       activeOptions={{ exact: true }}
-      onMouseOver={replay}
-      onFocus={replay}
+      onMouseOver={handleReplay}
+      onFocus={handleReplay}
     >
-      {/*0{index + 1}_ <span className='inline-block ml-2'>{link.label}</span>*/}
-      {link.label}
+      <span ref={indexRef} className='group-hover:bg-text group-hover:text-background'>
+        0{index + 1}_
+      </span>{' '}
+      <span ref={ref}>{link.label}</span>
+      {/*{link.label}*/}
     </Link>
   );
 }
