e182e3977e932a7010f6b8dfbfa03abe07841432
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-08-07 19:49:35 +0530

    ui: colors when visible

--- src/features/header/components/base-header-v2.tsx
+++ src/features/header/components/base-header-v2.tsx
@@ -5,7 +5,7 @@ import { type INavLink, NAV_LINKS } from './data';
 export function BaseHeaderV2() {
   return (
     <header className='fixed top-0 right-0 px-[2ch] py-[2ch]'>
-      <nav className='flex items-start justify-start gap-0 flex-col w-25.25 font-chivo font-medium'>
+      <nav className='flex items-start justify-start gap-0 flex-col w-26.75 font-chivo font-medium'>
         {NAV_LINKS.map((link, index) => (
           <NavLink key={link.id} link={link} index={index} />
         ))}
@@ -16,7 +16,7 @@ export function BaseHeaderV2() {
 
 function NavLink({ link, index }: { link: INavLink; index: number }) {
   const { ref: indexRef, replay: indexReplay } = useScramble({
-    text: `0${index}_`,
+    text: `0${index + 1}_`,
     speed: 0.6, // Explicitly set the base speed (default is 1)
     tick: 1, // Reduce this so it advances quickly along the string
     step: 1, // Ensures it introduces 1 new character position per redraw
@@ -42,7 +42,7 @@ function NavLink({ link, index }: { link: INavLink; index: number }) {
   return (
     <Link
       key={link.id}
-      className='focus-visible:outline-text/60 w-full group focus-visible:outline-1 uppercase transition-colors duration-300 py-[0.35ch]'
+      className='focus-visible:outline-text/65 w-full group focus-visible:outline-1 uppercase transition-colors duration-300 p-[0.35ch]'
       {...link.linkOptions}
       activeProps={{ className: 'text-text' }}
       inactiveProps={{ className: 'text-text/65 hover:text-text/90' }}
@@ -50,10 +50,11 @@ function NavLink({ link, index }: { link: INavLink; index: number }) {
       onMouseOver={handleReplay}
       onFocus={handleReplay}
     >
-      <span ref={indexRef} className='group-hover:bg-text group-hover:text-background'>
-        0{index + 1}_
-      </span>{' '}
-      <span ref={ref}>{link.label}</span>
+      <span
+        ref={indexRef}
+        className='group-hover:bg-secondary-background group-hover:text-secondary-text group-focus-visible:bg-secondary-background group-focus-visible:text-secondary-text'
+      />{' '}
+      <span ref={ref} />
       {/*{link.label}*/}
     </Link>
   );
