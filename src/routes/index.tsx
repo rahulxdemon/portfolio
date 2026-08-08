88e395400792e2d62c1b5766bb607f6497f46894
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-08-08 19:56:16 +0530

    feat: add more intro content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className='max-w-160 mx-6 sm:mx-27.5'>
+    <div className='max-w-158 mx-6 sm:mx-27.5'>
       <div className='py-20'>
         <div className=''>
           <div className='animate-fade-in-up [animation-delay:200ms] opacity-0'>
@@ -14,11 +14,14 @@ function Index() {
               <div className='text-base font-semibold'>
                 <p>Rahul Palamarthi</p>
               </div>
-              <div className='space-y-3 font-medium leading-6.5'>
+              <div className='space-y-4 font-medium leading-6.5'>
                 <p className='text-text/80'>
                   I'm a software developer at Nxtagent. From the start, I've approached the role more like a{' '}
-                  <span className='font-semibold italic text-text'>design engineer</span> focused on the product itself and the craft of painting
-                  every pixel.
+                  <span className='font-semibold italic'>design engineer</span> focused on the product itself and the craft of painting every pixel.
+                </p>
+                <p className='text-text/80'>
+                  I'm driven, sharp, and deeply focused on startups. I've worked as a founding engineer, putting early-stage products at the center of
+                  everything I do.
                 </p>
                 <p className='text-text/80'>Liked what you found? I’m on X, GitHub, and LinkedIn.</p>
               </div>
