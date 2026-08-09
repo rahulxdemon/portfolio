d646be7d8a4cfd0bc8b66d5ad91b2cc2c6478f27
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-08-09 07:57:01 +0530

    ui: animation delay function

--- src/features/shared/components/page-container.tsx
+++ src/features/shared/components/page-container.tsx
@@ -2,7 +2,7 @@ export function PageContainer({ children }: React.PropsWithChildren) {
   return (
     <div className='max-w-158 mx-6 sm:mx-27.5'>
       <div className='py-20'>
-        <div className='animate-fade-in-up [animation-delay:200ms] opacity-0'>{children}</div>
+        <div className='animate-fade-in-up [animation-delay:100ms] opacity-0 blur-md'>{children}</div>
       </div>
     </div>
   );
