1144997931f793484b48efa38391c7c3104e692f
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-19 22:33:40 +0530

    ui: minor work section styles

--- src/features/work/base-work.tsx
+++ src/features/work/base-work.tsx
@@ -2,7 +2,7 @@ import { WORK_EXPERIENCE } from '@/features/shared/data/cv';
 
 export function BaseWork() {
   return (
-    <div className='space-y-8 py-8'>
+    <div className='space-y-7.5 py-8'>
       <div className='text-sm font-semibold'>
         <p>Work</p>
       </div>
@@ -10,7 +10,7 @@ export function BaseWork() {
         {WORK_EXPERIENCE.map((work, i) => (
           <div
             key={i}
-            className='group/parent hover:px-2 hover:opacity-100 hover:text-text transition-[padding,opacity] pb-2.5 group-hover/wrapper:opacity-70 flex items-center justify-between gap-4 duration-300'
+            className='group/parent hover:px-2 hover:opacity-100 hover:text-text transition-[padding,opacity] py-1.25 group-hover/wrapper:opacity-60 flex items-center justify-between gap-4 duration-300'
           >
             <h2 className='text-sm pb-1'>{work.company}</h2>
             <div className='text-xs font-medium text-neutral-400 group-hover/parent:text-neutral-600 transition-colors duration-300'>{work.year}</div>
