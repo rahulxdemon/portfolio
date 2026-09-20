2360991666da3a6023beb4425a4becc0bed23139
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-20 15:32:18 +0530

    chore: add parent div to nav dock

--- src/features/shared/components/nav-dock.tsx
+++ src/features/shared/components/nav-dock.tsx
@@ -3,27 +3,29 @@ import { type INavLink, NAV_LINKS } from '../../header/components/data';
 
 export function NavDock() {
   return (
-    <nav
-      style={{
-        backdropFilter: 'url("#svgfilter")',
-      }}
-      className='font-baskerville fixed bottom-0 w-full h-14 z-30'
-    >
-      <div className='flex items-center justify-start gap-6 max-w-158 mx-auto h-full'>
-        {NAV_LINKS.filter((link) => link.isEnabled).map((link) => (
-          <NavLink key={link.id} link={link} />
-        ))}
+    <nav className='font-baskerville fixed bottom-0 w-full h-14 z-30'>
+      <div
+        style={{
+          backdropFilter: 'url("#svgfilter")',
+        }}
+        className='h-full'
+      >
+        <div className='flex items-center justify-start gap-6 max-w-158 mx-auto h-full'>
+          {NAV_LINKS.filter((link) => link.isEnabled).map((link) => (
+            <NavLink key={link.id} link={link} />
+          ))}
+        </div>
+        {/** biome-ignore lint/a11y/noSvgWithoutTitle: intended behaviour */}
+        <svg xmlns='http://www.w3.org/2000/svg' width='0' height='0'>
+          <defs>
+            <filter id='svgfilter'>
+              <feTurbulence type='fractalNoise' baseFrequency='0.3' numOctaves='1' seed='0' stitchTiles='stitch' result='feTurbulence-391ac80d' />
+              <feGaussianBlur in='SourceGraphic' stdDeviation='2' edgeMode='duplicate' result='feGaussianBlur-85e146a6' />
+              <feDisplacementMap in='feGaussianBlur-85e146a6' in2='feTurbulence-391ac80d' scale='37' xChannelSelector='A' yChannelSelector='A' />
+            </filter>
+          </defs>
+        </svg>
       </div>
-      {/** biome-ignore lint/a11y/noSvgWithoutTitle: intended behaviour */}
-      <svg xmlns='http://www.w3.org/2000/svg' width='0' height='0'>
-        <defs>
-          <filter id='svgfilter'>
-            <feTurbulence type='fractalNoise' baseFrequency='0.3' numOctaves='1' seed='0' stitchTiles='stitch' result='feTurbulence-391ac80d' />
-            <feGaussianBlur in='SourceGraphic' stdDeviation='2' edgeMode='duplicate' result='feGaussianBlur-85e146a6' />
-            <feDisplacementMap in='feGaussianBlur-85e146a6' in2='feTurbulence-391ac80d' scale='37' xChannelSelector='A' yChannelSelector='A' />
-          </filter>
-        </defs>
-      </svg>
     </nav>
   );
 }
