632fb31bcb16e75f38cf5f17adafcb85b205abd7
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-21 19:08:44 +0530

    refactor: remove unnecessary files

--- src/client/providers/router-devtools.tsx
+++
@@ -1,7 +0,0 @@
-import { TanStackRouterDevtools as _DevTools } from '@tanstack/react-router-devtools';
-
-export function TanStackRouterDevtools() {
-  if (import.meta.env.MODE === 'production') return null;
-
-  return <_DevTools />;
-}
