205a6ee580f3f810faeae5e8350ac989d7edb2e9
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-22 11:27:54 +0530

    feat: integrate fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,34 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Plus Jakarta Sans',
+      cssVariable: '--font-jakarta',
+      fallbacks: ['serif'],
+      weights: [200, 800],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Libre Baskerville',
+      cssVariable: '--font-baskerville',
+      fallbacks: ['monospace'],
+      weights: [400, 700],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Chivo Mono',
+      cssVariable: '--font-chivo',
+      fallbacks: ['monospace'],
+      weights: [200, 900],
+    },
+  ],
   server: {
     port: 3000,
   },
