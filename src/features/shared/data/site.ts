fd08f014f924965a8e249b8b4c77117d281a2db7
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-22 20:13:16 +0530

    feat: common site config

--- src/features/shared/data/site.ts
+++ src/features/shared/data/site.ts
@@ -3,6 +3,7 @@ interface ISite {
   title: string;
   description: string;
   ogImage: string;
+  siteName: string;
 }
 
 export const SITE: ISite = {
@@ -10,4 +11,5 @@ export const SITE: ISite = {
   title: 'Rahul Palamarthi',
   description: 'The personal website of Rahul Palamarthi - Product & craft',
   ogImage: 'https://rahulgg.in/assets/og.png',
+  siteName: 'rahulgg.in',
 };
