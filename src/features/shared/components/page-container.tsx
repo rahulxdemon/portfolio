9e998b8aa5e250d033bb75a2cc198d4b866c51ea
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-09-22 12:09:44 +0530

    refactor: rewrite aboute page to astro

--- src/features/shared/components/page-container.tsx
+++
@@ -1,9 +0,0 @@
-export function PageContainer({ children }: React.PropsWithChildren) {
-  return (
-    <div className='max-w-158 mx-auto'>
-      <div className='pt-20 pb-30'>
-        <div className='animate-fade-in-up [animation-delay:100ms] opacity-0 blur-md'>{children}</div>
-      </div>
-    </div>
-  );
-}
